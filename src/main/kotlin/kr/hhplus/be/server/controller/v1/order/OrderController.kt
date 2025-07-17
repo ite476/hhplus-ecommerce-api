1c20d0b3027782ca4ca3f2b813c3c3ce041eb43f
성승현 <ite476@gmail.com>
2025-07-18 03:48:58 +0900

    feat(order): 주문 도메인 컨트롤러 API 더미데이터 반환 추가

--- src/main/kotlin/kr/hhplus/be/server/controller/v1/order/OrderController.kt
+++ src/main/kotlin/kr/hhplus/be/server/controller/v1/order/OrderController.kt
@@ -125,5 +125,19 @@ class OrderController {
     fun createOrder(
         @RequestHeader userId: Long,
         @RequestBody request: PostOrderRequestBody
-    ): ResponseEntity<PostOrderResponse> = ResponseEntity.ok(null)
+    ): ResponseEntity<PostOrderResponse> = ResponseEntity.ok(PostOrderResponse(
+        orderId = 1,
+        totalAmount = 40000,
+        usedPoint = 37000,
+        couponDiscountAmount = 3000,
+        orderItems = listOf(
+            OrderItemResponse(
+                productId = 1,
+                productName = "사탕",
+                unitPrice = 8000,
+                quantity = 5,
+                totalPrice = 40000,
+            )
+        )
+    ))
 }
