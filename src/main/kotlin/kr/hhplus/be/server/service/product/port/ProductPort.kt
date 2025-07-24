56b7f01a159309a086c2e88a99a82b68dd633fba
성승현 <ite476@gmail.com>
2025-07-25 06:56:22 +0900

    feat(product): 상품 도메인 port 인터페이스

---
+++ src/main/kotlin/kr/hhplus/be/server/service/product/port/ProductPort.kt
@@ -0,0 +1,20 @@
+package kr.hhplus.be.server.service.product.port
+
+import kr.hhplus.be.server.service.product.entity.Product
+import kr.hhplus.be.server.service.product.entity.ProductSaleSummary
+import java.time.Duration
+import java.time.ZonedDateTime
+
+interface ProductPort {
+    fun findAllProducts() : List<Product>
+
+    fun findAllPopularProducts (
+        whenSearch: ZonedDateTime,
+        searchPeriod: Duration,
+        fetchSize: Int
+    ) : List<ProductSaleSummary>
+
+    fun findProductById(productId: Long) : Product
+    fun saveProduct(product: kr.hhplus.be.server.service.product.entity.Product)
+    fun existsProduct(productId: Long): Boolean
+}
