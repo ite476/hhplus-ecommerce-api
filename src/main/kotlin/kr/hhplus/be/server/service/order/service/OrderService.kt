ce6332f9feef316a5488059c8dd1d2764242c163
성승현 <ite476@gmail.com>
2025-07-28 04:37:08 +0900

    fix(Order): OrderService.kt 인터페이스 추출 및 도메인 구현 수정
    > Order.id, OrderItem.id를 nullable로 변경
    > 주문 프로세스를 단계별로 메서드 분리
    > 1. 주문 생성 맥락에 필요한 객체 로딩
    > 2. 주문 생성 및 보상 트랜잭션 처리
    > 명명된 매개변수 적극 도입

--- src/main/kotlin/kr/hhplus/be/server/service/order/service/OrderService.kt
+++ src/main/kotlin/kr/hhplus/be/server/service/order/service/OrderService.kt
@@ -1,27 +1,44 @@
 package kr.hhplus.be.server.service.order.service
 
-import kr.hhplus.be.server.service.coupon.service.CouponService
+import kr.hhplus.be.server.service.coupon.entity.UserCoupon
+import kr.hhplus.be.server.service.coupon.usecase.FindUserCouponByIdUsecase
+import kr.hhplus.be.server.service.coupon.usecase.UseUserCouponUsecase
 import kr.hhplus.be.server.service.order.entity.Order
+import kr.hhplus.be.server.service.order.entity.OrderItem
 import kr.hhplus.be.server.service.order.port.DataPlatformPort
 import kr.hhplus.be.server.service.order.port.OrderPort
-import kr.hhplus.be.server.service.point.service.PointService
+import kr.hhplus.be.server.service.order.usecase.CreateOrderUsecase
+import kr.hhplus.be.server.service.point.usecase.ChargePointUsecase
+import kr.hhplus.be.server.service.point.usecase.UsePointUsecase
+import kr.hhplus.be.server.service.product.entity.Product
 import kr.hhplus.be.server.service.product.entity.ProductSale
-import kr.hhplus.be.server.service.product.service.ProductService
+import kr.hhplus.be.server.service.product.usecase.AddProductStockUsecase
+import kr.hhplus.be.server.service.product.usecase.FindProductByIdUsecase
+import kr.hhplus.be.server.service.product.usecase.ReduceProductStockUsecase
 import kr.hhplus.be.server.service.transaction.CompensationScope
-import kr.hhplus.be.server.service.user.service.UserService
+import kr.hhplus.be.server.service.user.entity.User
+import kr.hhplus.be.server.service.user.usecase.FindUserByIdUsecase
 import kr.hhplus.be.server.util.KoreanTimeProvider
 import org.springframework.stereotype.Service
+import java.time.ZonedDateTime
 
 @Service
-class OrderService (
-    val userService: UserService,
-    val pointService: PointService,
-    val productService: ProductService,
-    val couponService: CouponService,
+class OrderService(
+    val findUserByIdUsecase: FindUserByIdUsecase,
+    val findProductByIdUsercase: FindProductByIdUsecase,
+    val reduceProductStockUsecase: ReduceProductStockUsecase,
+    val addProduceStockUsecase: AddProductStockUsecase,
+    val usePointUsecase: UsePointUsecase,
+    val chargePointUsecase: ChargePointUsecase,
+    val findUserCouponByIdUsecase: FindUserCouponByIdUsecase,
+    val useUserCouponUsecase: UseUserCouponUsecase,
     val orderPort: OrderPort,
     val dataPlatformPort: DataPlatformPort,
-    val timeProvider: KoreanTimeProvider,
-) {
+    val timeProvider: KoreanTimeProvider
+) : CreateOrderUsecase {
+    /**
+     * 주문 생성 Input
+     */
     data class CreateOrderInput(
         val userId: Long,
         val products: List<ProductWithQuantity>,
@@ -33,94 +50,123 @@ class OrderService (
         )
     }
 
-    /**
-     * 주문을 생성합니다.
-     */
-    suspend fun createOrder(
+    override suspend fun createOrder(
         input: CreateOrderInput
     ) : Order {
-        userService.requireUserExists(input.userId)
-        input.products.forEach { productService.requireProductExists(it.productId) }
-        if(input.userCouponId != null) couponService.requireUserCouponExists(input.userCouponId)
+        val context: OrderCreationContext = OrderCreationContext.prepare(
+            input = input,
+            now = timeProvider.now(),
+            findUserByIdUsecase = findUserByIdUsecase,
+            findProductByIdUsercase = findProductByIdUsercase,
+            findUserCouponByIdUsecase = findUserCouponByIdUsecase
+        )
 
-        val now = timeProvider.now()
-        val scope = CompensationScope()
+        val order: Order = CompensationScope.runTransaction {
+            createOrder(context)
+        }
 
-        try{
-            // 회원 정보 획득 (포인트 잔고 조회 겸용)
-            val user = userService.readSingleUser(input.userId)
+        return order
+    }
 
-            // 각 상품 별
-            // - 재고 차감
-            // - 현재 상태 스탬프 처리
-            val productsStamp = input.products.map {
-                val product = productService.readSingleProduct(it.productId)
+    /**
+     * 주문 생성 시 필요한 도메인 객체 모음
+     */
+    private data class OrderCreationContext(
+        val now: ZonedDateTime,
+        val user: User,
+        val productSales: List<ProductSale>,
+        val orderItems: List<OrderItem>,
+        val usedCoupon: UserCoupon?
+    ) {
+        companion object {
+            fun prepare(
+                input: CreateOrderInput,
+                now: ZonedDateTime,
+                findUserByIdUsecase: FindUserByIdUsecase,
+                findProductByIdUsercase: FindProductByIdUsecase,
+                findUserCouponByIdUsecase: FindUserCouponByIdUsecase
+            ): OrderCreationContext {
+                val user: User = findUserByIdUsecase.findUserById(userId = input.userId)
 
-                scope.execute {
-                    productService.reduceProductStock(it.productId, it.quantity, now)
-                }.compensate {
-                    productService.addProductStock(it.productId, it.quantity, now)
+                val productSales: List<ProductSale> = input.products.map { sale ->
+                    val product: Product = findProductByIdUsercase.findProductById(productId = sale.productId)
+                    ProductSale(product = product, soldCount = sale.quantity, soldAt = now)
                 }
 
-                ProductSale(
-                    product,
-                    it.quantity,
-                    now,
-                )
-            }
-
-            // 총 가격 계산 시작
-            var totalPrice : Long = productsStamp.sumOf { it.product.price * it.soldCount }
-
-            // 쿠폰 사용 시
-            // - 쿠폰 차감
-            // - 총 가격에서 차감 (할인)
-            if (input.userCouponId != null){
-                val coupon = couponService.readSingleUserCoupon(
-                    input.userId,
-                    input.userCouponId)
-
-                scope.execute {
-                    couponService.useUserCoupon(coupon, now)
-                }.compensate {
-                    couponService.rollbackUserCouponUsage(coupon, now)
+                val orderItems: List<OrderItem> = productSales.map { sale ->
+                    val productId: Long = sale.product.requiresId()
+                    OrderItem(productId = productId, productName = sale.product.name, unitPrice = sale.product.price, quantity = sale.soldCount)
                 }
 
-                totalPrice -= coupon.discount
-            }
+                val usedCoupon: UserCoupon? = input.userCouponId?.let {
+                    findUserCouponByIdUsecase.findUserCouponById(userId = input.userId, userCouponId = it)
+                }
 
-            // 최종 가격을 회원 포인트 잔고에서 차감
-            scope.execute {
-                pointService.usePoint(user.id, totalPrice)
+                return OrderCreationContext(now = now, user = user, productSales = productSales, orderItems = orderItems, usedCoupon = usedCoupon)
+            }
+        }
+    }
+
+    /**
+     * 주문 생성 (보상 트랜잭션 진입)
+     */
+    private suspend fun CompensationScope.createOrder(
+        context: OrderCreationContext
+    ) : Order {
+        // 각 상품 별 재고 차감
+        context.productSales.forEach { sale ->
+            val product: Product = sale.product
+            val productId: Long = product.requiresId()
+
+            val quantity: Int = sale.soldCount
+            val now: ZonedDateTime = context.now
+
+
+            execute {
+                reduceProductStockUsecase.reduceProductStock(productId = productId, quantity = quantity, now = now)
             }.compensate {
-                pointService.chargePoint(user.id, totalPrice)
+                addProduceStockUsecase.addProductStock(productId = productId, quantity = quantity, now = now)
             }
-
-            // 주문 생성
-            val orderResult = scope.execute {
-                orderPort.createOrder(
-                    user,
-                    input.userCouponId,
-                    productsStamp,
-                    now
-                )
-            }
-
-            val order = orderResult.result
-
-            orderResult.compensate {
-                orderPort.cancelOrder(order)
-            }
-
-            // 주문 정보를 외부 데이터 플랫폼으로 전송
-            dataPlatformPort.sendOrderData(order)
-
-            return order
         }
-        catch (ex: Exception){
-            scope.rollbackAll()
 
-            throw ex
+        // 쿠폰 사용 시 쿠폰 차감
+        context.usedCoupon?.let { userCoupon ->
+            val now: ZonedDateTime = context.now
+
+            execute {
+                useUserCouponUsecase.useUserCoupon(userCoupon = userCoupon, now = now)
+            }.compensate {
+                useUserCouponUsecase.rollbackUserCouponUsage(userCoupon = userCoupon, now = now)
+            }
         }
+
+        // 총 금액 계산
+        val totalPrice: Long = run {
+            val productsPrice: Long = context.orderItems.sumOf { it.totalPrice }
+            val discountedPrice: Long = (context.usedCoupon?.discount ?: 0L)
+
+            productsPrice - discountedPrice
+        }
+
+        // 포인트 차감 처리
+        val userId: Long = context.user.requiresId()
+
+        execute {
+            usePointUsecase.usePoint(userId = userId, point = totalPrice)
+        }.compensate {
+            chargePointUsecase.chargePoint(userId = userId, point = totalPrice)
+        }
+
+        // 주문 생성
+        val order: Order = execute {
+            orderPort.createOrder(user = context.user, userCouponId = context.usedCoupon?.id, productsStamp = context.productSales, now = context.now)
+        }.compensateBy { order ->
+            orderPort.cancelOrder(order = order)
+        }
+
+        // 주문 정보를 외부 데이터 플랫폼으로 전송
+        dataPlatformPort.sendOrderData(order)
+
+        return order
     }
 }
