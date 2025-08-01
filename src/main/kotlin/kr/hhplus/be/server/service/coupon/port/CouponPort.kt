6d8b75949c2cb8eab63b796b63c3f2c2d0dd8938
성승현 <ite476@gmail.com>
2025-08-01 09:39:36 +0900

    fix(Coupon): 쿠폰 회수 시 시간 입력 받도록 수정

--- src/main/kotlin/kr/hhplus/be/server/service/coupon/port/CouponPort.kt
+++ src/main/kotlin/kr/hhplus/be/server/service/coupon/port/CouponPort.kt
@@ -3,13 +3,13 @@ package kr.hhplus.be.server.service.coupon.port
 import kr.hhplus.be.server.service.coupon.entity.UserCoupon
 import kr.hhplus.be.server.service.pagination.PagedList
 import kr.hhplus.be.server.service.pagination.PagingOptions
+import java.time.ZonedDateTime
 
 interface CouponPort {
     fun findUserCouponById(userId: Long, userCouponId: Long): UserCoupon
     fun saveUserCoupon(userCoupon: UserCoupon)
-    fun findAllUserCoupons(userId: Long) : List<UserCoupon>
-    fun issueCoupon(couponId: Long) : UserCoupon
-    fun revokeCoupon(issuedUserCoupon: kr.hhplus.be.server.service.coupon.entity.UserCoupon)
+    fun issueCoupon(userId: Long, couponId: Long, now: ZonedDateTime) : UserCoupon
+    fun revokeCoupon(issuedUserCoupon: UserCoupon, now: ZonedDateTime)
     fun existsUserCoupon(userCouponId: Long) : Boolean
     fun existsCoupon(couponId: Long): Boolean
     fun findPagedUserCoupons(userId: Long, pagingOptions: PagingOptions): PagedList<UserCoupon>
