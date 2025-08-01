ac689e0ba1371b5cbf72c2a96cf227692e9769b9
성승현 <ite476@gmail.com>
2025-08-01 09:49:54 +0900

    feat(Order): 주문 Adapter, 데이터 수집 더미 Adapter

--- src/main/kotlin/kr/hhplus/be/server/repository/order/DummyDataPlatformAdapter.kt
+++ src/main/kotlin/kr/hhplus/be/server/repository/order/DummyDataPlatformAdapter.kt
@@ -1,12 +1,14 @@
 package kr.hhplus.be.server.repository.order
 
+import kotlinx.coroutines.delay
 import kr.hhplus.be.server.service.order.entity.Order
 import kr.hhplus.be.server.service.order.port.DataPlatformPort
 import org.springframework.stereotype.Component
 
 @Component
-class DataPlatformTemporaryAdapter : DataPlatformPort {
+class DummyDataPlatformAdapter : DataPlatformPort {
     override suspend fun sendOrderData(order: Order) {
-        TODO("Not yet implemented")
+        // 외부 데이터 플랫폼 전송 시뮬레이션
+        delay(100)  // 0.1초 지연
     }
 }
